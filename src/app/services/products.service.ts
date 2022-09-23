84a0a8fd136ed615b4dc5ab2a96a35756a726bbf
Camila Zupanovich <cami.zupanovich@hotmail.com>
2022-09-23 12:54:51 -0300

    services

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -1,5 +1,7 @@
 import { Injectable } from "@angular/core";
-
+//servicio para pasar datos a los componentes, 
+// *** IMPORTANTE ***
+//VER CON REDUX
 @Injectable()
 export class ProductsService{
     constructor(){}
