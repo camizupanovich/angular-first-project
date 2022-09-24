93dfa38e774a522818af2a9b560422f182ddfcb8
Camila Zupanovich <cami.zupanovich@hotmail.com>
2022-09-24 16:29:01 -0300

    exploring *ngIf , *ngFor and services

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -1,11 +1,21 @@
-import { Component } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
+import { Product,ProductsService } from "src/app/services/products.service";
 
 @Component({
     selector: 'about-component',
     templateUrl: './about.component.html'
 })
-export class AboutComponent{
-    constructor(){
+export class AboutComponent implements OnInit{
+    products:Product[]=[];
+    viewProduct:Product[]=[];
+    constructor(private _productsService: ProductsService){
         console.log('About working successfully')
+    }
+    ngOnInit(): void {
+        this.products= this._productsService.getProducts();
+    }
+    getImage(id:number){
+        return this.viewProduct = this.products.filter(p=>p.id===id)
+        console.log(this.viewProduct)
     }
 }
